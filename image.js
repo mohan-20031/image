3a05ae35eae2aa0e4b2a80f653fc55579c2cf2f8
mohan-20031 <mohamohan406@gmail.com>
2024-07-11 15:27:49 +0530

    Add files via upload

---
+++ image.js
@@ -0,0 +1,7 @@
+const imageSources = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg'];
+function changeBackgroundImage() {
+const randomIndex = Math.floor(Math.random() * imageSources.length);
+document.body.style.backgroundImage = `url(${imageSources[randomIndex]})`;
+}
+// Change the background image every 1 seconds (1000 milliseconds)
+setInterval(changeBackgroundImage, 1000);
